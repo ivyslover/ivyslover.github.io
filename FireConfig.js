88725fc6cc677d287fd9b16bfb4ffc141c5c6d4c
Bilal <67349145+ivyslover@users.noreply.github.com>
2024-01-17 12:39:50 +0300

    Add files via upload

---
+++ FireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+apiKey: "AIzaSyCR6vYIt2VJEwHM1-QxfX4BMrXBztDEx2c",
+authDomain: "ivyslover-7faf4.firebaseapp.com",
+databaseURL: "https://ivyslover-7faf4-default-rtdb.europe-west1.firebasedatabase.app",
+projectId: "ivyslover-7faf4",
+storageBucket: "ivyslover-7faf4.appspot.com",
+messagingSenderId: "191878649174",
+appId: "1:191878649174:web:cb54a1fe8976dc7265f2f7"
+    };
+
+    // Firebase'i başlat
+    firebase.initializeApp(firebaseConfig);
